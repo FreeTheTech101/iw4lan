f75c1ea3bab55f0de699134381519e7dee66ee48
win32 <win32re@gmail.com>
2014-12-31 15:36:14 +0100

    fixed release configuration build and removed unused defines

--- premake5.lua
+++ premake5.lua
@@ -16,14 +16,13 @@ solution "iw4lan"
 	configuration "Release*"
 		targetdir "bin/release"
 		defines "NDEBUG"
-		optimize "Speed"
 			
 	project "clientdll"
 		targetname "iw4lan"
 		language "C++"
 		kind "SharedLib"
 
-		defines { "WIN32", "NDEBUG", "_WINDOWS", "_WINDLL", "_USRDLL", "STEAM_API_EXPORTS", "PRE_RELEASE_DEMO", "D3D_EXPEROMINTS", "OLD_CRUFTY_CRUFT" }
+		defines { "WIN32", "NDEBUG", "_WINDOWS", "_WINDLL", "_USRDLL", "STEAM_API_EXPORTS" }
 
 		flags { "NoMinimalRebuild" }
 		
@@ -39,7 +38,7 @@ solution "iw4lan"
 		
 		libdirs { "deps/lib/" }
 	
-		links { "ws2_32.lib", "winmm.lib", "libcurl.lib", "wldap32.lib", "dbghelp.lib", "pdcurses.lib", "shlwapi.lib" }
+		links { "ws2_32.lib", "winmm.lib", "wldap32.lib", "dbghelp.lib", "shlwapi.lib", "libcurl.lib", "pdcurses.lib" }
 
 		configuration "windows"
-			linkoptions "/DYNAMICBASE:NO /SAFESEH:NO /LARGEADDRESSAWARE"
+			linkoptions "/DYNAMICBASE:NO /SAFESEH:NO"
